ee65a4c7bb7eda94524317164e183672db7dd6c3
Ryan <ryanabelaits@gmail.com>
2020-11-18 12:56:48 +1100

    -1.2 Update
    -Added Birch
    -Added Movement
    -Added boundaries

--- Doodle Jump Clone/Assets/Scripts/PLAYER.cs
+++ Doodle Jump Clone/Assets/Scripts/PLAYER.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PLAYER : MonoBehaviour
 {
-
+    #region Variables
     Rigidbody2D rigi;
     Transform playerTrans;
 
@@ -14,20 +15,58 @@ public class PLAYER : MonoBehaviour
 
     public static Vector2 position = new Vector2();
 
-    
+    public float speed;
+    public float boundary;
+    public float teleportPos;
 
+    int score;
 
-    // Start is called before the first frame update
+    public Text scoreText;
+
+    #endregion
+    #region Start
     void Start()
     {
+
         rigi = GetComponent<Rigidbody2D>();
         playerTrans = GetComponent<Transform>();
+
+        
         
     }
-
-    // Update is called once per frame
+    #endregion
+    #region Update 
     void Update()
     {
+        #region Boundaries
         position = transform.position;
+
+        if(position.x <= -boundary)
+        {
+            position.x = -teleportPos;
+        }
+        if (position.x >= boundary)
+        {
+            position.x = teleportPos;
+        }
+
+        transform.position = position;
+
+        #endregion
+        #region Inputs
+        if (Input.GetKey(KeyCode.A))
+        {
+            rigi.AddForce(playerTrans.right * -speed);
+        }
+        if (Input.GetKey(KeyCode.D))
+        {
+            rigi.AddForce(playerTrans.right * speed);
+        }
+        #endregion
+        #region Scoring
+        int score = (int)position.y;
+        scoreText.text = "" + score;
+        #endregion
     }
+    #endregion
 }
