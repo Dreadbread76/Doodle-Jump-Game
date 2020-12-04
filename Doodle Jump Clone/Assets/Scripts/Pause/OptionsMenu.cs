57439c145cfcb2e65d915b8d57a60d58c3a8d99b
Dreadbread76 <61036310+Dreadbread76@users.noreply.github.com>
2020-12-05 01:35:15 +1100

    Doodle Jump Game

---
+++ Doodle Jump Clone/Assets/Scripts/Pause/OptionsMenu.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+
+public class OptionsMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    #region Audio
+
+    public AudioMixer mixer;
+    public AudioClip[] audioClips;
+    public AudioClip currentClips;
+    public AudioSource audioSource;
+    public void SetVolume(float volume)
+    {
+        mixer.SetFloat("volume", volume);
+        PlayerPrefs.SetFloat("savedVolume",volume);
+        PlayerPrefs.Save();
+    }
+    public void Start()
+    {
+        PlayClip();
+        mixer.SetFloat("volume", PlayerPrefs.GetFloat("savedVolume"));
+        
+    }
+    public void PlayClip()
+    {
+        audioSource.clip = audioClips[Random.Range(0, audioClips.Length)];
+        audioSource.Play();
+    }
+    #endregion
+}
---
+++ Doodle Jump Clone/Assets/Scripts/Pause/PauseMenu.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseMenu : MonoBehaviour
+{
+    #region Variables
+    public static bool isPaused;
+    public GameObject pausePanel;
+   
+    
+
+
+    
+
+    #endregion
+    #region Start
+    void Start()
+    {
+        Time.timeScale = 1;
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+        isPaused = false;
+        pausePanel.SetActive(false);
+       
+
+
+    }
+    #endregion
+    #region Update
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            PauseMenuActive();
+        }
+    }
+    #endregion
+    #region Pause Menu
+    public void PauseMenuActive()
+    {
+        
+        isPaused = !isPaused;
+        Debug.Log(isPaused);
+
+        //Bring up menu and pause game when paused
+        if (isPaused)
+        {
+
+            Time.timeScale = 0;
+            Cursor.lockState = CursorLockMode.None;
+            Cursor.visible = true;
+            pausePanel.SetActive(true);
+            
+        }
+        //Remove menu and unpause game 
+        else
+        {
+           
+            Time.timeScale = 1;
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+            pausePanel.SetActive(false);
+        }
+    }
+    #endregion
+
+
+}
